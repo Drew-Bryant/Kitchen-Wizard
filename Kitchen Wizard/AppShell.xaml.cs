2c8fcb72afcbc20ab9d05196cb778bd00433f126
Drew Bryant <32604066+Nofxthepirate@users.noreply.github.com>
2023-01-06 17:47:39 -0800

    Add project files.

---
+++ Kitchen Wizard/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Kitchen_Wizard;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Kitchen Wizard/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Kitchen_Wizard;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
